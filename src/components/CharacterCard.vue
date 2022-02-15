f5e813b70c34bad5a09e287cc765dd0e1511f7c5
EstelleThvn <estelle.thouvenin00@gmail.com>
2022-02-15 17:28:34 +0100

    [Characters] Add characterCard

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="CharacterCard">
+    <h1>Hello Wooorld</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterCard',
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Characters.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Characters">
+    <CharacterCard />
+  </div>
+</template>
+
+<script>
+import CharacterCard from './CharacterCard.vue'
+
+export default {
+  name: 'Characters',
+  components: {
+    CharacterCard,
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
