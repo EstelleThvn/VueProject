b94e3fc04366bd10e94506bfcb69e4f4a552826d
EstelleThvn <estelle.thouvenin00@gmail.com>
2022-03-01 19:45:40 +0100

    [API] Hide api key

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,7 @@
 const getAllCharacters = async function() {
     const response = await fetch('https://the-one-api.dev/v2/character', {
         headers: {
-            Authorization: 'Bearer C88kljpP2ckgIDw5axzu'
+            Authorization: `Bearer ${process.env.VUE_APP_API_KEY}`
         },
     })
     if(response.status == 200){
@@ -15,7 +15,7 @@ const getAllCharacters = async function() {
 const getCharacter = async function() {
     const response = await fetch('https://the-one-api.dev/v2/character/5cdbdecb6dc0baeae48cfa59', {
         headers: {
-            Authorization: 'Bearer C88kljpP2ckgIDw5axzu'
+            Authorization: `Bearer ${process.env.VUE_APP_API_KEY}`
         },
     })
     if(response.status == 200){
@@ -29,7 +29,7 @@ const getCharacter = async function() {
 const getAllQuotes = async function() {
     const response = await fetch('https://the-one-api.dev/v2/quote', {
         headers: {
-            Authorization: 'Bearer C88kljpP2ckgIDw5axzu'
+            Authorization: `Bearer ${process.env.VUE_APP_API_KEY}`
         },
     })
     if(response.status == 200){
